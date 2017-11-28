a423d2341b147bdf814fc0da74795c146c4594ec
nakos <nakos.al@iasa.gr>
2017-11-28 19:00:58 +0200

    Pass task response cache option to unique task registry

--- src/lib/isql/utils/UniqueTaskRegistry.js
+++ src/lib/isql/utils/UniqueTaskRegistry.js
@@ -6,14 +6,21 @@ function taskCompletionHandler(uniqTask) {
 }
 
 export default class UniqueTaskRegistry {
-  constructor(name) {
+  constructor(name, {cacheResponseTime = 0} = {cacheResponseTime: 0}) {
     this._name = name;
     this._registry = {};
     this._onTaskEnded = taskCompletionHandler.bind(this);
+    this._options = {
+      cacheResponseTime: cacheResponseTime
+    };
   }
 
   register(taskId, taskCaller) {
-    let task = new UniqueTask({taskId, taskCaller, onTaskEnded: this._onTaskEnded});
+    let task = new UniqueTask({
+      taskId, taskCaller,
+      onTaskEnded: this._onTaskEnded,
+      cacheResponseTime: this._options.cacheResponseTime
+    });
     let id = task.getId();
     if (!this._registry[id]) {
       this._registry[id] = task;
