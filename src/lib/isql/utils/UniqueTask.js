b1a654b181571b9492a3668d7cafd41a2480893f
nakos <nakos.al@iasa.gr>
2017-11-28 19:00:10 +0200

    Fix typos and add response data caching in UniqeuTask instances

--- src/lib/isql/utils/UniqueTask.js
+++ src/lib/isql/utils/UniqueTask.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 
 class UniqueTask {
-  constructor({id, taskCaller, onTaskEnded}) {
-    this._id = _.trim(_.isPlainObject(id) ? JSON.stringify(id) : id);
+  constructor({taskId, taskCaller, onTaskEnded, cacheResponseTime = 0}) {
+    this._id = _.trim(_.isPlainObject(taskId) ? JSON.stringify(taskId) : taskId);
     this._taskCaller = taskCaller;
     this._promise = Promise.resolve();
     this._subscribers = [];
@@ -10,6 +10,7 @@ class UniqueTask {
     this._taskResultStatus = null;
     this._taskResult = null;
     this._onTaskEnded = onTaskEnded;
+    this._cacheResponseTime = parseInt(cacheResponseTime) || 0;
   }
 
   getTaskStatus() {
@@ -38,33 +39,65 @@ class UniqueTask {
     return this._taskCaller;
   }
 
+  _execTask(taskCaller) {
+    return new Promise((resolve, reject) => {
+      try {
+        let res = taskCaller();
+        if (res && _.isFunction(res.then) && _.isFunction(res.catch)) {
+          res.then(resolve);
+        } else {
+          resolve(res);
+        }
+      } catch (e) {
+        if (res && _.isFunction(res.then) && _.isFunction(res.catch)) {
+          res.catch(reject);
+        } else {
+          reject(e);
+        }
+      }
+    });
+    return this._taskCaller();
+  }
+
   _resolveHandler(res) {
     this._taskResultStatus = UniqueTask.TASK_SUCCESS;
+    this._taskResult = res;
     this._fullfilledHandler();
-    this._subscribers.forEach(subscriber => subscribe.onResolve(res));
+    this._subscribers.forEach(subscriber => subscriber.onResolve(res));
   }
 
   _rejectHandler(err) {
     this._taskResultStatus = UniqueTask.TASK_ERROR;
+    this._taskResult = err;
     this._fullfilledHandler();
-    this._subscribers.forEach(subscriber => subscribe.onReject(err));
+    this._subscribers.forEach(subscriber => subscriber.onReject(err));
   }
 
   _fullfilledHandler() {
     this._taskStatus = UniqueTask.STATUS_ENDED;
-    this._onTaskEnded(this);
+    let cacheFor = this.getCacheResponseTime();
+    if (cacheFor > 0) {
+      setTimeout(function() {this._onTaskEnded(this);}.bind(this), cacheFor);
+    } else {
+      this._onTaskEnded(this);
+    }
+
+  }
+
+  getCacheResponseTime() {
+    return this._cacheResponseTime;
   }
 
   start() {
     if (this._taskStatus !== UniqueTask.STATUS_RUNNING) {
       this._taskStatus = UniqueTask.STATUS_RUNNING;
-      this.getTaskCaller().then(_resolveHandler.bind(this)).catch(_rejectHandler.bind(this));
+      this._execTask(this.getTaskCaller()).then(this._resolveHandler.bind(this)).catch(this._rejectHandler.bind(this));
     }
   }
 
   subscribe() {
     if (this.getTaskStatus() === UniqueTask.STATUS_ENDED) {
-      return this.getTaskResponse();
+      return this.getTaskResult();
     }
 
     let subscriber = {
