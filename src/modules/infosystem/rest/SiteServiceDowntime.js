96090b102928fb1737921b1dd9492394604f19de
nakos <nakos.al@iasa.gr>
2018-01-16 16:49:26 +0200

    Add missing file for REST API gocdb downtimes resource

---
+++ src/modules/infosystem/rest/SiteServiceDowntime.js
@@ -0,0 +1,116 @@
+import _ from 'lodash';
+import {query, TEMPLATE_COLLECTION_HEADER} from './restModel';
+import {filterToGraphQL, asyncFilterToGraphQL, resultHandlerByPath} from './utils';
+import gql from 'graphql-tag';
+import {TEMPLATE_SITE_SERVICE_DETAILS_FIELDS}  from './SiteService';
+import {TEMPLATE_SITE_DETAILS_FIELDS} from './Site';
+
+export const TEMPLATE_SITE_SERVICE_DOWNTIME_ITEM_FIELDS = `
+  id
+  downtimePKey
+  classification
+  severity
+  startDate
+  endDate
+  formatedStartDate
+  formatedEndDate
+  serviceType
+  gocPortalUrl
+  outcome
+`;
+
+export const TEMPLATE_SITE_SERVICE_DOWNTIME_DETAILS_FIELDS =`
+  id
+  downtimePKey
+  classification
+  severity
+  startDate
+  endDate
+  formatedStartDate
+  formatedEndDate
+  serviceType
+  gocPortalUrl
+  outcome
+  site {
+    id
+    pkey
+    name
+  }
+  service {
+    id
+    endpointPKey
+    endpointURL
+  }
+`;
+
+export const TEMPLATE_SITE_SERVICE_DOWNTIME_COLLECTION_FIELDS = `
+  items {
+    ${TEMPLATE_SITE_SERVICE_DOWNTIME_ITEM_FIELDS}
+  }
+`;
+
+export const getCallerByIdentifier = (id, onlyQuery = false) => {
+  if (onlyQuery) {
+    return `id: {eq: "${id}"}`;
+  }else {
+    return `SiteServiceDowntimeById(id: "${id}")`;
+  }
+};
+
+export const getByIdentifier = (id) => {
+  let caller = getCallerByIdentifier(id);
+
+  return query(gql`{
+    data: ${caller} {
+      ${TEMPLATE_SITE_SERVICE_DOWNTIME_DETAILS_FIELDS}
+    }
+  }`);
+};
+
+export const getSite = (id) => {
+  let caller = getCallerByIdentifier(id);
+  return query(gql`{
+    data: ${caller} {
+      id
+      site {
+        ${TEMPLATE_SITE_DETAILS_FIELDS}
+      }
+    }
+  }`).then(resultHandlerByPath('data.site as data'));
+};
+
+export const getSiteService = (id, imageId) => {
+  let caller = getCallerByIdentifier(id);
+
+  return query(gql`{
+    data: ${caller} {
+      id
+      service {
+        ${TEMPLATE_SITE_SERVICE_DETAILS_FIELDS}
+      }
+    }
+  }`).then(resultHandlerByPath('data.siteService as data'));
+};
+
+export const getAll = ({filter = {}, limit = 0, skip = 0} = {filter:{}, limit: 0, skip: 0}) => {
+  return asyncFilterToGraphQL(filter).then(flt => {
+    return query(gql`
+      {
+        data: SiteServiceDowntimes(filter: ${flt}, limit: ${limit}, skip: ${skip}) {
+          ${TEMPLATE_COLLECTION_HEADER}
+          ${TEMPLATE_SITE_SERVICE_DOWNTIME_COLLECTION_FIELDS}
+        }
+      }
+    `);
+  });
+};
+
+
+
+export default {
+  getAll,
+  getCallerByIdentifier,
+  getByIdentifier,
+  getSite,
+  getSiteService
+}
