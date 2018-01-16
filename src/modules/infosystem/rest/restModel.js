87fb5b5f41f503bebaefd591c016802f1485d369
nakos <nakos.al@iasa.gr>
2018-01-16 14:30:49 +0200

    Minor textual change

--- src/modules/infosystem/rest/restModel.js
+++ src/modules/infosystem/rest/restModel.js
@@ -2,7 +2,7 @@ import { GraphQLClient } from 'graphql-request'
 import getGraphQLEndpoint from './getGraphQLEndpoint';
 
 const _graphQLEndpoint = getGraphQLEndpoint();
-console.log('REST GRPAPHQL ENDPOJNT', _graphQLEndpoint);
+console.log('REST GRPAPHQL ENDPOINT', _graphQLEndpoint);
 export const client = new GraphQLClient(_graphQLEndpoint,{ headers: {}});
 
 export const query = (query, variables) => {
